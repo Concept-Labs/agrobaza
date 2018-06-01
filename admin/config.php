62ce9080d8c2f6f8f29c2ee087950067d1ab8c0a
SeeAllcom <yavbulavin@gmail.com>
2018-06-02 02:41:23 +0300

    поставив українізацію

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://agrobaza/admin/');
+define('HTTP_CATALOG', 'http://agrobaza/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://agrobaza/admin/');
+define('HTTPS_CATALOG', 'http://agrobaza/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/agrobaza/admin/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/agrobaza/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/agrobaza/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/agrobaza/admin/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/agrobaza/admin/view/template/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/agrobaza/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/agrobaza/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/agrobaza/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/agrobaza/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/agrobaza/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/agrobaza/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OSPanel/domains/agrobaza/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'agrobaza');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://agrobaza/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://agrobaza/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/agrobaza/catalog/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/agrobaza/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/agrobaza/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/agrobaza/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/agrobaza/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/agrobaza/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/agrobaza/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/agrobaza/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/agrobaza/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/agrobaza/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/agrobaza/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'agrobaza');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
