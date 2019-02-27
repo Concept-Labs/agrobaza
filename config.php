cb37528b7e40da96114803943dc48b99a0a38d73
StingerWin <witalikwin@gmail.com>
2019-02-27 20:47:54 +0200

    родпр

--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://agrobaza/');
 define('HTTPS_SERVER', 'http://agrobaza/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/OSPanel/domains/agrobaza/catalog/');
-define('DIR_SYSTEM', 'D:/OSPanel/domains/agrobaza/system/');
-define('DIR_IMAGE', 'D:/OSPanel/domains/agrobaza/image/');
-define('DIR_LANGUAGE', 'D:/OSPanel/domains/agrobaza/catalog/language/');
-define('DIR_TEMPLATE', 'D:/OSPanel/domains/agrobaza/catalog/view/theme/');
-define('DIR_CONFIG', 'D:/OSPanel/domains/agrobaza/system/config/');
-define('DIR_CACHE', 'D:/OSPanel/domains/agrobaza/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/OSPanel/domains/agrobaza/system/storage/download/');
-define('DIR_LOGS', 'D:/OSPanel/domains/agrobaza/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/OSPanel/domains/agrobaza/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/OSPanel/domains/agrobaza/system/storage/upload/');
+define('DIR_APPLICATION', ROOT_DIR.'catalog/');
+define('DIR_SYSTEM', ROOT_DIR.'system/');
+define('DIR_IMAGE', ROOT_DIR.'image/');
+define('DIR_LANGUAGE', DIR_APPLICATION.'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION.'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM.'config/');
+define('DIR_CACHE', DIR_SYSTEM.'storage/cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM.'storage/download/');
+define('DIR_LOGS', DIR_SYSTEM.'storage/logs/');
+define('DIR_MODIFICATION', DIR_SYSTEM.'storage/modification/');
+define('DIR_UPLOAD', DIR_SYSTEM.'storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+
 // Version
 define('VERSION', '2.3.0.2.3');
+define('DIR_SEP', '/');
+define('ROOT_DIR', getcwd(). DIR_SEP);
 
 // Configuration
 if (is_file('config.php')) {
@@ -9,11 +12,11 @@ if (is_file('config.php')) {
 
 // Install
 if (!defined('DIR_APPLICATION')) {
-	header('Location: install/index.php');
+	header('Location: install'.DIR_SEP.'index.php');
 	exit;
 }
-
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
+
 
 start('catalog');
